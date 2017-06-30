5c50873fa0418f0cd8ac4e1f95682eb9a1721cd3
simonhfrost <simonhfrost@gmail.com>
2017-06-30 22:55:55 +0200

    Random lines example

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -10,21 +10,22 @@ function setup() {
   noStroke();
 }
 
+var GLOBAL_RANDOM_POINT = [Math.random() * WIDTH, Math.random() * HEIGHT];
+var GLOBAL_COLOR = Math.random() * 100;
+
+setInterval(function() {
+  GLOBAL_RANDOM_POINT = [Math.random() * WIDTH, Math.random() * HEIGHT];
+}, 500);
+
 function draw() {
   noFill();
 
-  stroke(255, 150, 0);
-  strokeWeight(Math.random() * 4);
+  stroke(150 + 105 * Math.random(), GLOBAL_COLOR);
+  strokeWeight(Math.random() * 2);
 
-  var randomStart = Math.random() *  HALF_PI;
-  var bigness = Math.random();
+  const RANDOM_POINT = [50 - 100 * Math.random(), 50 - 100 * Math.random()];
+  const fuzzinessX = 20 * Math.random();
+  const fuzzinessY = 20 * Math.random();
 
-  arc(
-    Math.random() * WIDTH,
-    Math.random() * HEIGHT,
-    200 * bigness,
-    200 * bigness,
-    randomStart + HALF_PI,
-    randomStart + HALF_PI + bigness * PI
-  );
+  line(RANDOM_POINT[0], RANDOM_POINT[1], WIDTH + fuzzinessX + GLOBAL_RANDOM_POINT[0], HEIGHT + fuzzinessY + GLOBAL_RANDOM_POINT[1]);
 }
