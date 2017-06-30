9a15a0bea4f421c92944f7765bb8762296209a79
simonhfrost <simonhfrost@gmail.com>
2017-06-30 22:36:19 +0200

    First example with orange arcs

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,27 +1,30 @@
 var xoff = 0.0;
 var xincrement = 0.01;
 
+const WIDTH = 800;
+const HEIGHT = 600;
+
 function setup() {
-  createCanvas(710, 400);
+  createCanvas(WIDTH, HEIGHT);
   background(0);
   noStroke();
 }
 
 function draw() {
-  // Create an alpha blended background
-  fill(0, 10);
-  rect(0,0,width,height);
+  noFill();
 
-  // float n = random(0,width);  // Try this line instead of noise
+  stroke(255, 150, 0);
+  strokeWeight(Math.random() * 4);
 
-  // Get a noise value based on xoff and scale
-  // it according to the window's width
-  var n = noise(xoff)*width;
+  var randomStart = Math.random() *  HALF_PI;
+  var bigness = Math.random();
 
-  // With each cycle, increment xoff
-  xoff += xincrement;
-
-  // Draw the ellipse at the value produced by perlin noise
-  fill(200);
-  ellipse(n,height/2, 64, 64);
+  arc(
+    Math.random() * WIDTH,
+    Math.random() * HEIGHT,
+    200 * bigness,
+    200 * bigness,
+    randomStart + HALF_PI,
+    randomStart + HALF_PI + bigness * PI
+  );
 }
