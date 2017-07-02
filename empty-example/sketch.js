2e067a16a087f3e8a668cc697e1ebc80cc7116db
simonhfrost <simonhfrost@gmail.com>
2017-07-02 16:27:03 +0200

    Bezier curves intro

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -4,19 +4,33 @@ const MIDDLE = [WIDTH / 2, HEIGHT / 2];
 
 var setup = function() {
   createCanvas(WIDTH, HEIGHT);
-  background(175, 255, 175);
+  background(0);
   noFill();
   stroke(255);
+  strokeWeight(4);
 }
 
 var current = 0;
 var next = 50;
+var strokeValue = 255;
+var strokeWeightValue = 4;
+
+var offset = 0;
+setInterval(function() {
+  current = 0;
+  next = 50;
+  offset += 50;
+  stroke(255, 255, strokeValue);
+  strokeValue -= 10;
+  strokeWeight(strokeWeightValue);
+  strokeWeightValue -= 0.3;
+}, 1000);
 
 var draw = function() {
   beginShape();
 
-  bezier(current, 0, current, 50, next, 50, next, 0);
-  // bezier(current, 50, current, 0, next, 0, next, 50);
+  bezier(current, offset + 0, current, offset + 50, next, offset + 50, next, offset + 0);
+  bezier(current, offset + 50, current, offset + 0, next, offset + 0, next, offset + 50);
 
   current += 50;
   next += 50;
