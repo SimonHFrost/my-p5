57eb3f09b39a841726d85095465f2e3762c3608a
simonhfrost <simonhfrost@gmail.com>
2017-07-02 16:32:09 +0200

    Prepare for new example

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -7,30 +7,15 @@ var setup = function() {
   background(0);
   noFill();
   stroke(255);
-  strokeWeight(4);
+  strokeWeight(1);
 }
-
-var current = 0;
-var next = 50;
-var strokeValue = 255;
-var strokeWeightValue = 4;
-
-var offset = 0;
-setInterval(function() {
-  current = 0;
-  next = 50;
-  offset += 50;
-  stroke(255, 255, strokeValue);
-  strokeValue -= 10;
-  strokeWeight(strokeWeightValue);
-  strokeWeightValue -= 0.3;
-}, 1000);
 
 var draw = function() {
   beginShape();
 
-  bezier(current, offset + 0, current, offset + 50, next, offset + 50, next, offset + 0);
-  bezier(current, offset + 50, current, offset + 0, next, offset + 0, next, offset + 50);
+  var current = 0;
+  var next = 50;
+  bezier(current, 0, current, 50, next, 50, next, 0);
 
   current += 50;
   next += 50;
