f02f5f6a7b7766184ebdd5f3fca88ecae14df7f3
simonhfrost <simonhfrost@gmail.com>
2017-08-11 22:48:55 +0200

    10x10 grid

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -13,8 +13,8 @@ var setup = function() {
 
 var draw = function() {
   // NOTE: Offset by 1 so there's padding at the side of the screen
-  for (let x = 1; x < 7; x++) {
-    for (let y = 1; y < 7; y++) {
+  for (let x = 1; x < 11; x++) {
+    for (let y = 1; y < 11; y++) {
       const first = [x * GRID_SIZE, y * GRID_SIZE]
       const second = [first[0] + 10, first[1] + 10];
 
