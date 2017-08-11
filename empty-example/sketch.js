511c47b4e33aac8c7096e280f4c985c11662aea2
simonhfrost <simonhfrost@gmail.com>
2017-08-11 23:04:37 +0200

    Substitute a square for one entry

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -2,13 +2,16 @@ const WIDTH = 800;
 const HEIGHT = 600;
 
 const GRID_SIZE = 25;
+const TARGET = [2, 2];
 
 var setup = function() {
   createCanvas(WIDTH, HEIGHT);
   background(0);
-  noFill();
+  fill(255);
+
   stroke(255);
   strokeWeight(1);
+  strokeCap(SQUARE);
 }
 
 var draw = function() {
@@ -16,6 +19,13 @@ var draw = function() {
   for (let x = 1; x < 11; x++) {
     for (let y = 1; y < 11; y++) {
       const first = [x * GRID_SIZE, y * GRID_SIZE]
+
+      if (x === TARGET[0] && y === TARGET[1]) {
+        // NOTE: Offset by half the distance of the line (5) kind of hacky...
+        rect(first[0] + 2.5, first[1] + 2.5, 5, 5);
+        continue;
+      }
+
       const second = [first[0] + 10, first[1] + 10];
 
       line(first[0], first[1], second[0], second[1]);
