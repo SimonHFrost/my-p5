998f9b52fac0ded34ed50a244857e5a4a32b844f
simonhfrost <simonhfrost@gmail.com>
2017-08-27 13:48:45 +0200

    failed rotation example

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -31,4 +31,12 @@ var draw = function() {
       line(first[0], first[1], second[0], second[1]);
     }
   }
+
+  testRotation();
+}
+
+var testRotation = () => {
+  rotate(-45);
+  translate(300, 300);
+  rect(0, 0, 1, 20);
 }
