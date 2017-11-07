d0dcc92e30e2c74e9b6b807f0a55f8e8de82bcdf
simonhfrost <simonhfrost@gmail.com>
2017-11-07 12:07:20 +0100

    Put square at target

--- sketch.js
+++ sketch.js
@@ -6,6 +6,8 @@ const SPACING = 25;
 const RECT_SIZE_X = 15;
 const RECT_SIZE_Y = 2;
 
+const SQUARE_SIZE = 4;
+
 var setup = function() {
   createCanvas(WIDTH, HEIGHT);
   background(0);
@@ -16,6 +18,13 @@ var setup = function() {
   stroke(255);
   strokeWeight(1);
   strokeCap(SQUARE);
+}
+
+var drawSquare = (posX, posY) => {
+  push();
+  translate(posX, posY);
+  rect(-(SQUARE_SIZE/2), -(SQUARE_SIZE/2), SQUARE_SIZE, SQUARE_SIZE);
+  pop();
 }
 
 var drawRectangle = (posX, posY, angle) => {
@@ -32,8 +41,9 @@ var draw = function() {
 
   for (let x = 1; x < 32; x++) {
     for (let y = 1; y < 24; y++) {
-
-      if (x !== targetX || y !== targetY) {
+      if (x === targetX && y === targetY) {
+        drawSquare(x * SPACING, y * SPACING);
+      } else {
         const angle = Math.atan2(targetY - y, targetX - x);
         drawRectangle(x * SPACING, y * SPACING, angle);
       }
